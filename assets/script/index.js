ced3aac64e8d0edfe4c22ce4771628feda7a93d2
nekeoke <ukwuchinaekeoke@gmail.com>
2023-08-08 18:12:13 +0000

    commited changes

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,30 +1,33 @@
 const display = document.querySelector('#display')
 const image = document.querySelectorAll('.hairImg');
 
-let transition = display.style.transition = '.6s'
+const transition = (time) => display.style.transition = `${time}s`
+
+
+
 
 for (let i = 0; i < image.length; i++) {
     let img = image[i].id;
     let url = `url(assets/images/${img}.jpg)`
 
     image[i].onmouseover = function () {
-        if (i < image.length) {
-            display.style.backgroundImage = url
-            display.style.transform = 'scale(1.1)'
-            display.style.width = '94%'
-            transition
-        }
+        display.style.backgroundImage = url
+        display.style.transform = 'scale(1.1)'
+        display.style.width = '94%' 
+        transition(.5)        
     }
+
     image[i].onmouseout = () => {
         display.style.transform = 'scale(1)'
+        transition(2)
     }
 }
 display.onmouseover = () => {
-        transition
+        // transition
         display.style.transform = 'scale(1.1)'
 }
 display.onmouseout = () => {
-    transition
+    // transition
     display.style.transform = 'scale(1)'
 }
 
