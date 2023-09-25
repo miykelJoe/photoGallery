0123b63041c7836fade6cd4a7bebf47ed92d3bc9
nekeoke <ukwuchinaekeoke@gmail.com>
2023-09-25 15:42:30 +0000

    added codes on image name visibility

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,19 +1,37 @@
 const display = document.querySelector('#display')
 const image = document.querySelectorAll('.hairImg');
+const hairNmae = document.querySelectorAll('.hairstyle-name')
+const h1 = document.querySelector('.h1')
 
 const transition = (time) => display.style.transition = `${time}s`
+h1.innerText = '  PHOTO DISPLaY'.toUpperCase();
 
 
+
+function deleteRestOfWord(imageName, targetString){
+    const index = imageName.indexOf(targetString);
+
+    if (index !== -1){
+        return imageName.slice(0, index);
+    }else{
+        return imageName;
+    }
+}
 
 
 for (let i = 0; i < image.length; i++) {
     let img = image[i].id;
-    let url = `url(assets/images/${img}.jpg)`
+        let url = `url(assets/images/${img})`
 
+    hairNmae[i].innerText = deleteRestOfWord(img, '.')
+    
     image[i].onmouseover = function () {
+        h1.innerText = ''
+        
         display.style.backgroundImage = url
         display.style.transform = 'scale(1.1)'
         display.style.width = '94%' 
+        display.setAttribute("data-value", "yes")
         transition(.5)        
     }
 
@@ -22,22 +40,17 @@ for (let i = 0; i < image.length; i++) {
         transition(2)
     }
 }
+
 display.onmouseover = () => {
-        // transition
-        display.style.transform = 'scale(1.1)'
+        if(display.getAttribute("data-value"))
+            display.style.transform = 'scale(1.1)'
 }
 display.onmouseout = () => {
-    // transition
     display.style.transform = 'scale(1)'
 }
 
 display.onclick = () => {
     display.style.backgroundImage = 'none'
+    display.setAttribute("data-value", "")
+    h1.innerText = 'PHOTO DISPLAY'.toUpperCase();
 }
-
-// if (display.style.backgroundImage == ''){
-    // display.onmouseover = () => {
-        // console.log(2)
-    // display.style.backgroundColor = 'red'
-    // }
-// }
